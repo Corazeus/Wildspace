0755d88185c755063ec18552c4a6fa1ab098a6af
Renzeus Ruiz <ruizrenzeus@gmail.com>
2023-05-25 00:54:05 +0800

    Created Database and models

--- wil/settings.py
+++ wil/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'wil.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wildspacedb',
+        'Username': 'admin',
+        'Password': 'admin',
+        'host': '127.0.0.1',
+        'port': '3306',
+        'OPTIONS': {'init_command': "set sql_mode='strict_trans_tables'"},
     }
 }
 
