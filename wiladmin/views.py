eaf69ccf15aef293f32570c0b12e0e22c9079141
Renzeus Ruiz <ruizrenzeus@gmail.com>
2023-05-25 01:53:40 +0800

    Added Login Authenticate

--- wiladmin/views.py
+++ wiladmin/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
-def adminlogin(request):
-    return render(request, "wiladmin/login.html", {})
+#def adminlogin(request):
+#    return render(request, "wiladmin/login.html", {})
+
 def admindashbaord(request):
     return render(request, "wiladmin/dashboard.html", {})
+
 def walkindashboard(request):
     return render(request, "wiladmin/walkindashboard.html", {})
+
+def adminlogin(request):
+    
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.info(request, "Invalid Username or Password")
+            return redirect('adminlogin')
+    
+    else:
+        return render (request, "wiladmin/login.html", {})
