ec69678fd99f0b1e1691a3f06de858e38e7d6b37
Renzeus Ruiz <ruizrenzeus@gmail.com>
2023-05-25 13:32:42 +0800

    Updated settings.py and dashboard.html

--- wiladmin/views.py
+++ wiladmin/views.py
@@ -4,15 +4,8 @@ from django.contrib import messages
 from .models import User, WalkinBooking
 from django.views import View
 
-# Create your views here.
-#def adminlogin(request):
-#    return render(request, "wiladmin/login.html", {})
-
 def admindashbaord(request):
     return render(request, "wiladmin/dashboard.html", {})
-
-#def walkindashboard(request):
-#    return render(request, "wiladmin/walkindashboard.html", {})
 
 def adminlogin(request):
     
