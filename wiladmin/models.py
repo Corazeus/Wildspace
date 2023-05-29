1614ae2f206dd5c8e795ab9531f27728360627e0
Renzeus Ruiz <ruizrenzeus@gmail.com>
2023-05-30 00:59:37 +0800

    udpated Models.py

--- wiladmin/models.py
+++ wiladmin/models.py
@@ -9,6 +9,9 @@ class User(models.Model):
     def __str__(self):
         return self.username;
     
+    def getUserDetails(self):
+        return self.username, self.password, self.type;
+    
 class WalkinBooking(models.Model):
     bookingid = models.AutoField(primary_key=True);
     referenceid = models.CharField(max_length=20);
@@ -19,6 +22,9 @@ class WalkinBooking(models.Model):
     def __str__(self):
         return self.userid+"-"+self.referenceid;
     
+    def getWalkingBookings(self):
+        return self.bookingid, self.referenceid, self.userid, self.schedule, self.status;
+    
 class Logs(models.Model):
     logid = models.AutoField(primary_key=True);
     referenceid = models.CharField(max_length=20);
@@ -28,3 +34,6 @@ class Logs(models.Model):
         
     def __str__(self):
         return self.userid+"-"+self.referenceid+"-"+self.status;
+    
+    def getLogDetails(self):
+        return self.logid, self.referenceid, self.userid, self.datetime, self.status;
