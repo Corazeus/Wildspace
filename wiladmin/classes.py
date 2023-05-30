f4ae4e295f1d948d5403695b0a56e37d569f57b7
Renzeus Ruiz <ruizrenzeus@gmail.com>
2023-05-30 20:48:34 +0800

    created classes.py

---
+++ wiladmin/classes.py
@@ -0,0 +1,8 @@
+class WalkinBooking:
+
+    def __init__(self,bookingid,referenceid,userid,schedule,status):
+        self.bookingid = bookingid
+        self.referenceid = referenceid
+        self.userid = userid
+        self.schedule = schedule
+        self.status = status
