bbaefae69d9ffcb37ce7c963377178a52206faec
Kayken Gosuico <kaykengosuico@gmail.com>
2023-05-31 15:05:19 +0800

    ReOOP user side

--- polls/static/js/script.js
+++ polls/static/js/script.js
@@ -1,30 +1,28 @@
-var selectedAreaId;
 
-    function showMessage(message, button) {
-        var areaId = button ? button.getAttribute('data-areaid') : null;
-        selectedAreaId = areaId;
-        var popupMessage = document.getElementById('popupMessage');
-        var popupText = document.getElementById('popupText');
-        var buttonContainer = document.getElementById('buttonContainer');
-        var referenceContainer = document.getElementById('referenceContainer');
-        popupText.innerHTML = message;
-        buttonContainer.style.display = 'block';
-        referenceContainer.style.display = 'none';
-        popupMessage.style.display = 'block';
-    }
+function showMessage(message, button) {
+    var areaId = button ? button.getAttribute('data-areaid') : null;
+    selectedAreaId = areaId;
+    var popupMessage = document.getElementById('popupMessage');
+    var popupText = document.getElementById('popupText');
+    var buttonContainer = document.getElementById('buttonContainer');
+    var referenceContainer = document.getElementById('referenceContainer');
+    popupText.innerHTML = message;
+    buttonContainer.style.display = 'block';
+    referenceContainer.style.display = 'none';
+    popupMessage.style.display = 'block';
+}
 
-    function hideMessage() {
-        var popupMessage = document.getElementById('popupMessage');
-        popupMessage.style.display = 'none';
-    }
+function hideMessage() {
+    var popupMessage = document.getElementById('popupMessage');
+    popupMessage.style.display = 'none';
+}
 
-        function areaButtonClick(areaId) {
+function areaButtonClick(areaId) {
     fetch('/area_button_click/?area_id=' + areaId)
         .then(response => response.json())
         .then(data => {
             if (data.reference_number) {
                 showReferenceNumber(areaId, data.reference_number);
-
                 insertIntoDatabase(areaId, data.reference_number);
             }
         })
@@ -53,56 +51,42 @@ function insertIntoDatabase(areaId, referenceNumber) {
         });
 }
 
+function showReferenceNumber(areaId, referenceNumber) {
+    var referenceContainer = document.getElementById('referenceContainer');
+    var referenceText = areaId + referenceNumber.substring(2);
+    referenceContainer.textContent = referenceText;
+    referenceContainer.style.display = 'block';
+    showMessage('WALK-IN REFERENCE #: ' + referenceText);
+}
 
+function handleYesButtonClick() {
+    var referenceContainer = document.getElementById('referenceContainer');
+    var referenceNumber = generateReferenceNumber(selectedAreaId);
+    var areaId = referenceNumber.substring(0, 2);
 
-    function showReferenceNumber(areaId, referenceNumber) {
-        var referenceContainer = document.getElementById('referenceContainer');
-        var referenceText = areaId + referenceNumber.substring(2);
-        referenceContainer.textContent = referenceText;
+    if (selectedAreaId === areaId) {
+        var buttonContainer = document.getElementById('buttonContainer');
+        buttonContainer.style.display = 'none';
+        var yesButton = document.querySelector('#popupMessage .popup-button:nth-child(1)');
+        var noButton = document.querySelector('#popupMessage .popup-button:nth-child(2)');
+        yesButton.style.display = 'none';
+        noButton.style.display = 'none';
         referenceContainer.style.display = 'block';
-
-
-        showMessage('WALK-IN REFERENCE #: ' + referenceText);
+        referenceContainer.textContent = referenceNumber;
+        showMessage('WALK-IN REFERENCE #: ' + referenceNumber);
     }
+}
 
-    function handleYesButtonClick() {
-        var referenceContainer = document.getElementById('referenceContainer');
+function generateReferenceNumber(areaId) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/area_button_click/?area_id=' + areaId, false);
+    xhr.send();
 
-
-        var referenceNumber = generateReferenceNumber(selectedAreaId);
-
-        var areaId = referenceNumber.substring(0, 2);
-
-        if (selectedAreaId === areaId) {
-            var buttonContainer = document.getElementById('buttonContainer');
-            buttonContainer.style.display = 'none';
-
-
-            var yesButton = document.querySelector('#popupMessage .popup-button:nth-child(1)');
-            var noButton = document.querySelector('#popupMessage .popup-button:nth-child(2)');
-            yesButton.style.display = 'none';
-            noButton.style.display = 'none';
-
-
-            referenceContainer.style.display = 'block';
-            referenceContainer.textContent = referenceNumber;
-
-            showMessage('WALK-IN REFERENCE #: ' + referenceNumber);
-
-        }
+    if (xhr.status === 200) {
+        var response = JSON.parse(xhr.responseText);
+        return response.reference_number;
+    } else {
+        console.error('Error:', xhr.status);
+        return '';
     }
-
-    function generateReferenceNumber(areaId) {
-
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', '/area_button_click/?area_id=' + areaId, false);
-        xhr.send();
-
-        if (xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
-            return response.reference_number;
-        } else {
-            console.error('Error:', xhr.status);
-            return '';
-        }
-    }
+}
