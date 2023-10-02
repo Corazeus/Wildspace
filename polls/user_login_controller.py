90289f77c71fe11a99344e23492f9fcd92ef6e9f
Kayken Gosuico <kaykengosuico@gmail.com>
2023-10-02 20:14:47 +0800

    Userlogin, Userlogout sessions

--- polls/user_login_controller.py
+++ polls/user_login_controller.py
@@ -1,16 +1,39 @@
-from django.contrib.auth.views import LoginView
-from django.contrib import messages
 
-class UserLoginController(LoginView):
-    template_name = 'wil/userlogin.html'
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.forms import AuthenticationForm
 
-    def form_invalid(self, form):
-        messages.error(self.request, 'Invalid username or password')
-        return super().form_invalid(form)
+class UserLoginController(View):
+    template_name = "wil/userlogin.html"  
 
-    def form_valid(self, form):
-        messages.success(self.request, 'Logged in successfully')
-        return super().form_valid(form)
+    def get(self, request):
+        
+        form = AuthenticationForm()
+        context = {'form': form}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('user_dashboard')
+        
+        
+        context = {'form': form, 'error_message': 'Invalid credentials. Please try again.'}
+        return render(request, self.template_name, context)
+
+def user_logout(request):
+    logout(request)
+    return redirect('user_login')
+
 
 
 
