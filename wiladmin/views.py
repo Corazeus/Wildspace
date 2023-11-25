2384547f819f8f4125c46805196f068c501c61d5
Corazeus <ruizrenzeus@gmail.com>
2023-11-26 01:28:06 +0800

    Update: Cleaned code in View Workspaces auto-udd

--- wiladmin/views.py
+++ wiladmin/views.py
@@ -196,36 +196,38 @@ class BookGuestController(LoginRequiredMixin, View):
 class ViewWorkspacesController(LoginRequiredMixin, View):
     
     login_url = 'adminlogin'
-    countA1 = AssignedArea.objects.filter(area_id='A1').count()
-    countA2 = AssignedArea.objects.filter(area_id='A2').count()
-    countA3 = AssignedArea.objects.filter(area_id='A3').count()
-    countA4 = AssignedArea.objects.filter(area_id='A4').count()
-    countA5 = AssignedArea.objects.filter(area_id='A5').count()
-    countA6 = AssignedArea.objects.filter(area_id='A6').count()
-    countA7 = AssignedArea.objects.filter(area_id='A7').count()
-    countA8 = AssignedArea.objects.filter(area_id='A8').count()
-    countA9 = AssignedArea.objects.filter(area_id='A9').count()
     
     #Thsi will GET current count
     def GetAreaCount(self):
+        countA1 = AssignedArea.objects.filter(area_id='A1').count()
+        countA2 = AssignedArea.objects.filter(area_id='A2').count()
+        countA3 = AssignedArea.objects.filter(area_id='A3').count()
+        countA4 = AssignedArea.objects.filter(area_id='A4').count()
+        countA5 = AssignedArea.objects.filter(area_id='A5').count()
+        countA6 = AssignedArea.objects.filter(area_id='A6').count()
+        countA7 = AssignedArea.objects.filter(area_id='A7').count()
+        countA8 = AssignedArea.objects.filter(area_id='A8').count()
+        countA9 = AssignedArea.objects.filter(area_id='A9').count()
         
         area_count = [{
-            'countA1':self.countA1, 
-            'countA2':self.countA2, 
-            'countA3':self.countA3, 
-            'countA4':self.countA4, 
-            'countA5':self.countA5,
-            'countA6':self.countA6,
-            'countA7':self.countA7,
-            'countA8':self.countA8,
-            'countA9':self.countA9,
+            'countA1':countA1, 
+            'countA2':countA2, 
+            'countA3':countA3, 
+            'countA4':countA4, 
+            'countA5':countA5,
+            'countA6':countA6,
+            'countA7':countA7,
+            'countA8':countA8,
+            'countA9':countA9,
             }]
+        
         return area_count
     
     #This will get the area count into a JSON response
     #This function will be called in urls.py (url: wiladmin/workspaces)
     #Using the refresh.js this will be called in interval of 2 seconds
     #The refresh.js ajax will then replace the value with the new value
+    #P.S. made it not nested array for easy access in ajax
     def update_workspaces(request):
         countA1 = AssignedArea.objects.filter(area_id='A1').count()
         countA2 = AssignedArea.objects.filter(area_id='A2').count()
